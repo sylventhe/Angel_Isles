5ab0c8255d3d1290ff95085f2a668a142de0123e
Thomas Hendrick <18311229+sylventhe@users.noreply.github.com>
2017-12-19 08:02:05 -0500

    Initial Upload
    Initial upload of the base files for the map, generated by the FAF editor.

---
+++ angel_isles_scenario.lua
@@ -0,0 +1,22 @@
+version = 3
+ScenarioInfo = {
+    name = "Angel Isles Survival",
+    description = "Teamwork survival map, with the front player defending the objective, and the rear player performing eco.",
+    preview = '',
+    map_version = 0,
+    type = 'skirmish',
+    starts = true,
+    size = {512, 512},
+    map = '/maps/Angel_Isles/angel_isles.scmap',
+    save = '/maps/Angel_Isles/angel_isles_save.lua',
+    script = '/maps/Angel_Isles/angel_isles_script.lua',
+    norushradius = 40,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+            },
+            customprops = {
+            },
+        },
+    },
+}
